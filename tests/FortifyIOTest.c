c46a850f612eabbc630c9501b1cb45e68f761652
Christopher Bazley <119876938+chrisbazley@users.noreply.github.com>
2026-08-26 16:03:28 +0100

    Update FortifyIOTest.c to use optional FILE pointer

--- tests/FortifyIOTest.c
+++ tests/FortifyIOTest.c
@@ -7,26 +7,26 @@
 
 int main(void)
 {
-    FILE *stream = tmpfile();
+    _Optional FILE *stream = tmpfile();
     static const unsigned char data[] = {1, 2, 3, 4};
     unsigned char buffer[sizeof data];
 
     assert(stream != NULL);
-    assert(fwrite(data, sizeof data[0], sizeof data, stream) == sizeof data);
+    assert(fwrite(data, sizeof data[0], sizeof data, &*stream) == sizeof data);
     rewind(stream);
 
     Fortify_SetNumAllocationsLimit(0);
-    assert(fread(buffer, sizeof buffer[0], sizeof buffer, stream) == 0);
+    assert(fread(buffer, sizeof buffer[0], sizeof buffer, &*stream) == 0);
     Fortify_SetNumAllocationsLimit(ULONG_MAX);
 
-    assert(ferror(stream));
-    assert(ftell(stream) == 0);
-    clearerr(stream);
-    assert(!ferror(stream));
+    assert(ferror(&*stream));
+    assert(ftell(&*stream) == 0);
+    clearerr(&*stream);
+    assert(!ferror(&*stream));
 
-    assert(fread(buffer, sizeof buffer[0], sizeof buffer, stream) ==
+    assert(fread(buffer, sizeof buffer[0], sizeof buffer, &*stream) ==
            sizeof buffer);
-    assert(fclose(stream) == 0);
+    assert(fclose(&*stream) == 0);
     assert(Fortify_CheckAllMemory() == 0);
     return EXIT_SUCCESS;
 }
